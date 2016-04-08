01f025e8b69ef9c6443a5876ff2cee476db79bb0
DanielHolst <danielholst2@gmail.com>
2016-04-08 18:33:50 +0200

    find possible moves

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -11,11 +11,15 @@ using namespace std;
 class State
 {
 public:
-    State() = default;
-    State( vector<int> currentState ) : currentState_(currentState), prevState_(nullptr) {}
+    State () = default;
+    State ( vector<int> currentState, State* prevState = nullptr )
+            : currentState_(currentState), prevState_(prevState) {}
     
     vector<int> getBoard() const { return currentState_; }
     //void setBoard(vector<int> newBoard);
+    
+    bool compare(const State& s) const;
+    int getZeroPos() const;
     
     int countH1() const;
     bool checkBoard() const;
@@ -24,6 +28,27 @@ private:
     vector<int> currentState_;
     State* prevState_;
 };
+
+// compare two states
+bool State::compare(const State& s) const
+{
+    return currentState_ == s.currentState_;
+}
+
+// get the position of the empty block
+int State::getZeroPos() const
+{
+    for ( int i = 0; i < 9; i++ )
+    {
+        if ( currentState_.at(i) == 0 )
+        {
+            cout << "0 at pos: " << i << endl;
+            return i;
+        }
+    }
+    
+    return 0;
+}
 
 // returns the number of squares that are in the right place
 int State::countH1() const
@@ -45,24 +70,75 @@ bool State::checkBoard() const
 {
     vector<int> endBoard { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
     
-    for ( int i = 0; i < 9; i++)
-    {
-        if ( currentState_.at(i) != endBoard.at(i) )
-            return false;
-    }
-    return true;
+    return this->compare(State(endBoard));
 }
 
-// function to find the best next move
-//vector<int> findBestMove(vector<int> currentBoard) {}
+// print out values in vector
+void printVec(vector<int> vec)
+{
+    for (auto i : vec)
+        cout << i << " ";
+    cout << endl;
+    
+}
 
+// function to find possible moves
+vector<int> findMoves(State state)
+{
+    //find position of empty block
+    int pos = state.getZeroPos();
+    
+    vector<int> moves;
+    
+    switch (pos) {
+        case 0:
+            moves = { 1, 3 };
+            break;
+        case 1:
+            moves = {0,2,4};
+            break;
+        case 2:
+            moves = {1,5};
+            break;
+        case 3:
+            moves = {0,4,6};
+            break;
+        case 4:
+            moves = {1,3,5,7};
+            break;
+        case 5:
+            moves = {2,4,8};
+            break;
+        case 6:
+            moves = {3,7};
+            break;
+        case 7:
+            moves = {6,4,8};
+            break;
+        case 8:
+            moves = {5,7};
+            break;
+            
+        default:
+            cout << "error in findMoves" << endl;
+            break;
+    }
+    
+    return moves;
+}
 
 // solve the 8 puzzle and find the fastest way to solve it
 bool solve(State state)
 {
     cout << "number of correct boards from beginning: " << state.countH1() << endl;
-    
+    vector<int> possibleMoves;
     std::queue<State> q;
+    
+    q.push(state);
+    //find possible moves
+    possibleMoves = findMoves(q.front());
+    
+    printVec(possibleMoves);
     
     if (state.checkBoard())
         cout << "correct" << endl;
@@ -73,9 +149,16 @@ bool solve(State state)
     
 }
 
+//main function
 int main()
 {
     vector<int> startBoard { 1, 2, 3, 4, 5, 0, 7, 8, 6 };
+    /*
+     * 1 2 3
+     * 4 5 0
+     * 7 8 6
+     */
+    
     State state { startBoard };
     
     solve(state);
