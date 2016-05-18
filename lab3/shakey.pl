09d9e8f6897927f24c41cef533812f01355fee54
DanielHolst <danielholst2@gmail.com>
2016-05-18 14:23:29 +0200

    lab3 fixes

--- lab3/shakey.pl
+++ lab3/shakey.pl
@@ -21,27 +21,28 @@ act( climbDown(B),
     ).
 
 act( pushBox(B, Pos1, Pos2),
-    [shakey(S), block(B), at(S, Pos1), at(B, Pos1), lightOn(Pos1, true), on(S, floor)],
+    [shakey(S), block(B), connected(Pos1, Pos2), at(S, Pos1), at(B, Pos1), on(S, floor)],
     [at(S, Pos1), at(B, Pos1)],
     [at(S, Pos2), at(B, Pos2)]
     ).
 
 act( turnOn(),
-    [shakey(S), block(B), on(S, block), at(S, X), lightOn(X, false)],
-    [lightOn(X, false)],
-    [lightOn(X, true)]
+    [shakey(S), switch(L), block(B), at(B, X), on(S, block), at(S, X), at(L, X), lightOn(X, false)],
+    [lightOn(L, false)],
+    [lightOn(L, true)]
     ).
 
 act( turnOff(),
-    [shakey(S), block(B), on(S, block), at(S, X), lightOn(X, true)],
+    [shakey(S), block(B), at(B, X), on(S, block), at(S, X), lightOn(X, true)],
     [lightOn(X, true)],
     [lightOn(X, false)]
     ).
 
 % Goal state
 goal_state( [
-    %lightOn(room1, false),
-    at(block2, room2)
+    lightOn(room1_switch, false)
+    %at(block2, room2)
+    %at(s, room1)
     ]).
 
 % Initial state
@@ -62,17 +63,21 @@ initial_state( [
     connected(corridor, room3),
     connected(corridor, room4),
 
-    % switches
     connected(room1, room1_switch),
     connected(room2, room2_switch),
     connected(room3, room3_switch),
     connected(room4, room4_switch),
 
+    connected(room1_switch, room1),
+    connected(room2_switch, room2),
+    connected(room3_switch, room3),
+    connected(room4_switch, room4),
+
     % Lighs
-    lightOn(room1, true),
-    lightOn(room2, false),
-    lightOn(room3, false),
-    lightOn(room4, true),
+    lightOn(room1_switch, true),
+    lightOn(room2_switch, false),
+    lightOn(room3_switch, false),
+    lightOn(room4_switch, true),
 
     % blocks
     block(block1),
